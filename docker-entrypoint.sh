3b0cac28d0ebe015127283e79155de44a998ff37
Anish Adamane <93029923+GeniusPlums@users.noreply.github.com>
2024-12-03 02:15:13 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,18 +4,11 @@ set -e
 export SENTRY_RELEASE=$(cat SENTRY_RELEASE)
 
 echo "Running clickhouse-migrations"
-if [ -z "${CLICKHOUSE_PASSWORD}" ]; then
-    clickhouse-migrations migrate \
-        --host ${CLICKHOUSE_HOST:-localhost} \
-        --port ${CLICKHOUSE_PORT:-8123} \
-        --user ${CLICKHOUSE_USER:-default}
-else
-    clickhouse-migrations migrate \
-        --host ${CLICKHOUSE_HOST:-localhost} \
-        --port ${CLICKHOUSE_PORT:-8123} \
-        --user ${CLICKHOUSE_USER:-default} \
-        --password "${CLICKHOUSE_PASSWORD}"
-fi
+clickhouse-migrations migrate \
+    --host ${CLICKHOUSE_HOST:-localhost} \
+    --port ${CLICKHOUSE_PORT:-8123} \
+    --user ${CLICKHOUSE_USER:-default} \
+    --password "${CLICKHOUSE_PASSWORD:-}"
 
 echo "Running Typeorm migrations"
 typeorm-ts-node-commonjs migration:run -d packages/server/src/data-source.ts
